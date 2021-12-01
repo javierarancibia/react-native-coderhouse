33792b250b11c121fecbfb5cf29da129c3f42cac
Javier <javier.arancibia.reyes@hotmail.com>
2021-11-30 23:10:08 -0300

    fixed api

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,61 +1,103 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, Pressable } from "react-native";
-const config = require('../config')
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+  Image,
+  Pressable,
+} from "react-native";
+const config = require("../config");
 
 const HomeScreen = ({ navigation }) => {
-  const APP_ID = "03b6c229";
-  const API_KEY = "eaffd9d455270ea2f35fc2f0e8fe6d14";
   return (
     <View style={styles.screen}>
-      <Image
-        source={require("../assets/dinner.png")}
-        style={{
-          width: 190,
-          height: 190,
-        }}
-      />
-      <Text style={{ color: "black" }}>¿Que cocinaremos hoy?</Text>
-
-      <View style={styles.button}>
-        <Pressable
+      <View style={{ marginBottom: 25 }}>
+        <Image
+          source={require("../assets/cooking.jpg")}
+          style={{ resizeMode: "cover", height: 100, width: 200 }}
+        />
+      </View>
+      <View style={styles.screen}>
+        <TouchableOpacity
           onPress={() => {
             navigation.navigate("Option", {
               name: "Breakfast",
-              baseURL:
-                `https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=&app_id=${config.APP_ID}&app_key=${config.API_KEY}&mealType=Breakfast&random=true`,
+              baseURL: `https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=&app_id=${config.APP_ID}&app_key=${config.API_KEY}&mealType=Breakfast&random=true`,
             });
           }}
         >
-          <Text style={{ color: "white" }}> Desayuno</Text>
-        </Pressable>
-      </View>
+          <Image
+            source={require("../assets/cooking.jpg")}
+            style={{
+              height: 120,
+              width: 300,
+              borderRadius: 25,
+              marginBottom: 25,
+            }}
+          />
+          <Text
+            style={{
+              color: "white",
+              fontSize: 20,
+              position: "absolute",
+              marginBottom: 25,
+            }}
+          >
+            Desayuno
+          </Text>
+        </TouchableOpacity>
 
-      <View style={styles.button}>
-        <Pressable
+        <TouchableOpacity
           onPress={() => {
             navigation.navigate("Option", {
               name: "Lunch",
-              baseURL:
-              `https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=&app_id=${APP_ID}&app_key=${API_KEY}&mealType=Lunch&random=true`,
+              baseURL: `https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=&app_id=${config.APP_ID}&app_key=${config.API_KEY}&mealType=Lunch&random=true`,
             });
           }}
         >
-          <Text style={{ color: "white" }}>Almuerzo</Text>
-        </Pressable>
-      </View>
+          <Image
+            source={require("../assets/cooking.jpg")}
+            style={{
+              height: 120,
+              width: 300,
+              borderRadius: 25,
+              marginBottom: 25,
+            }}
+          />
+          <Text
+            style={{
+              color: "white",
+              fontSize: 20,
+              position: "absolute",
+              marginBottom: 25,
+            }}
+          >
+            Almuerzo
+          </Text>
+        </TouchableOpacity>
 
-      <View style={styles.button}>
-        <Pressable
+        <TouchableOpacity
           onPress={() => {
             navigation.navigate("Option", {
               name: "Dinner",
-              baseURL:
-              `https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=&app_id=${APP_ID}&app_key=${API_KEY}&mealType=Dinner&random=true`,
+              baseURL: `https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=&app_id=${config.APP_ID}&app_key=${config.API_KEY}&mealType=Dinner&random=true`,
             });
           }}
         >
-          <Text style={{ color: "white" }}>Cena</Text>
-        </Pressable>
+          <Image
+            source={require("../assets/cooking.jpg")}
+            style={{
+              height: 120,
+              width: 300,
+              borderRadius: 25,
+            }}
+          />
+          <Text style={{ color: "white", fontSize: 20, position: "absolute" }}>
+            Cena
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -66,7 +108,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
   },
   button: {
     alignItems: "center",
@@ -78,7 +119,16 @@ const styles = StyleSheet.create({
     backgroundColor: "orange",
     marginTop: 30,
     padding: 10,
-    fontFamily:'Lato-Regular'
+    fontFamily: "Lato-Regular",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
   },
 });
 
